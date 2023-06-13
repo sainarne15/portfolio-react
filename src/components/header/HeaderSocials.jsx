1e74a4677ff75a2df20660effbd9cf5f2e216c23
sainarne15 <sainarne15@gmail.com>
2023-06-13 12:39:19 -0400

    socials_update

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -7,8 +7,8 @@ import {FaLinkedin} from 'react-icons/fa';
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><FaGithub/></a>
+        <a href="https://www.linkedin.com/in/sainarne15/" target="_blank"><BsLinkedin/></a>
+        <a href="https://github.com/sainarne15" target="_blank"><FaGithub/></a>
        
         </div>
     );
